f020435dcc73935b1d15c943e353f8e3da6931c0
Amandeep Singh Sapra <sapraaman@gmail.com>
2018-05-04 13:16:46 +0530

    Fixed deployment script to point to new repository

--- config/deploy.rb
+++ config/deploy.rb
@@ -2,7 +2,7 @@
 lock "3.8.1"
 
 set :application, "newsframes"
-set :repo_url, "git@github.com:globalvoices/NewsFrames.git"
+set :repo_url, "git@github.com:globalvoices/NewsFrames-Code.git"
 
 # Default branch is :master
 # ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
